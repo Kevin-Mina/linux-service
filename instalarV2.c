2bb1e3f3180445dc8ed68e3859fdd14c88ade493
Kevin-Mina <101006020+Kevin-Mina@users.noreply.github.com>
2023-11-03 12:09:09 -0300

    Update instalarV2.c

--- instalarV2.c
+++ instalarV2.c
@@ -2,15 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// Conteúdo do arquivo linux.service
+const char* linux_service_content = 
+"[Unit]\n"
+"Description=linux\n"
+"\n"
+"[Service]\n"
+"Type=simple\n"
+"User=root\n"
+"ExecStart=/usr/bin/linux\n"
+"\n"
+"[Install]\n"
+"WantedBy=multi-user.target\n";
+
 int main() {
+    // Criar e escrever o conteúdo do arquivo linux.service
+    FILE *file = fopen("/etc/systemd/system/linux.service", "w");
+    if (file) {
+        fputs(linux_service_content, file);
+        fclose(file);
+    } else {
+        perror("Erro ao criar o arquivo linux.service");
+        return 1;
+    }
+
     // Copiar o arquivo "linux" para /usr/bin/
     if (access("/usr/bin/linux", F_OK) != 0) {
         system("sudo cp linux /usr/bin/");
-    }
-
-    // Copiar o arquivo "linux.service" para /etc/systemd/system/
-    if (access("/etc/systemd/system/linux.service", F_OK) != 0) {
-        system("sudo cp linux.service /etc/systemd/system/");
     }
 
     // Habilitar o serviço no systemd
